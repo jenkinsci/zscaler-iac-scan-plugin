972682832ba48e27a8818ef3ffb2317ac1866c62
Shyam Baitmangalkar <sbaitmangalkar@zscaler.com>
2022-07-07 16:44:34 +0530

    Mask access token in git URLs if present

--- src/main/java/io/jenkins/plugins/zscaler/SCMDetails.java
+++ src/main/java/io/jenkins/plugins/zscaler/SCMDetails.java
@@ -3,6 +3,7 @@ package io.jenkins.plugins.zscaler;
 import hudson.EnvVars;
 import io.jenkins.plugins.zscaler.models.BuildDetails;
 import io.jenkins.plugins.zscaler.models.SCMConstants;
+import org.apache.commons.lang.StringUtils;
 
 import java.net.URL;
 import java.util.logging.Level;
@@ -11,10 +12,12 @@ import java.util.logging.Logger;
 public class SCMDetails {
 
   private static final Logger LOGGER = Logger.getLogger(SCMDetails.class.getName());
+  private static final String MASK_STRING = "xxxxx";
   public static void populateSCMDetails(EnvVars env, BuildDetails buildDetails){
     String gitUrl = env.get(SCMConstants.GitUrl);
     if (gitUrl != null) {
       String repoUrl = gitUrl.substring(0,gitUrl.length()-4);
+      repoUrl = maskAccessToken(repoUrl);
       buildDetails.setRepoLoc(repoUrl);
       buildDetails.addAdditionalDetails(BuildDetails.scmType, gitUrl.contains("gitlab.com") ? SCMConstants.GITLAB : (gitUrl.contains("github.com") ? SCMConstants.GITHUB : "GIT"));
       buildDetails.setBranchName(env.get(SCMConstants.GitBranch));
@@ -36,5 +39,28 @@ public class SCMDetails {
       buildDetails.addAdditionalDetails(BuildDetails.scmType, SCMConstants.SVN);
       buildDetails.setCommitSha(env.get(SCMConstants.SvnRevision));
     }
+  }
+
+  /**
+   * Masks access token in git URL if present
+   *
+   * @param repoUrl
+   * @return
+   */
+  private static String maskAccessToken(String repoUrl) {
+    /*
+    If the repo URl is either of the following formats:
+    https://<token>@github/<username>/<repo>
+    or
+    https://oauth2:<token>@gitlab/<groupname>/<repo>
+    then, find the index of '@' and first '/'.
+    Then replace the string between 'https://' and '@' with mask string
+     */
+    int index = repoUrl.indexOf("@");
+    if (index > 0) {
+      int firstSlashIndex = repoUrl.indexOf("/");
+      repoUrl = StringUtils.replace(repoUrl, repoUrl.substring(firstSlashIndex + 2, index), MASK_STRING);
+    }
+    return repoUrl;
   }
 }
