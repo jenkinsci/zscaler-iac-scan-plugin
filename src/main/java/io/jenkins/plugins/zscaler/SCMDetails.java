c408067c6211be4d8e7f622a165b55a963c3d899
AparnaSaripaka <asaripaka@zscaler.com>
2023-01-10 16:47:38 +0530

    changes to handle multibranch pipeline repo details

--- src/main/java/io/jenkins/plugins/zscaler/SCMDetails.java
+++ src/main/java/io/jenkins/plugins/zscaler/SCMDetails.java
@@ -3,7 +3,7 @@ package io.jenkins.plugins.zscaler;
 import hudson.EnvVars;
 import io.jenkins.plugins.zscaler.models.BuildDetails;
 import io.jenkins.plugins.zscaler.models.SCMConstants;
-import org.apache.commons.lang.StringUtils;
+import org.json.JSONObject;
 
 import java.net.URL;
 import java.util.logging.Level;
@@ -12,10 +12,11 @@ import java.util.logging.Logger;
 public class SCMDetails {
 
   private static final Logger LOGGER = Logger.getLogger(SCMDetails.class.getName());
-  public static void populateSCMDetails(EnvVars env, BuildDetails buildDetails){
+
+  public static void populateSCMDetails(EnvVars env, BuildDetails buildDetails) {
     String gitUrl = env.get(SCMConstants.GitUrl);
     if (gitUrl != null) {
-      String repoUrl = gitUrl.substring(0,gitUrl.length()-4);
+      String repoUrl = gitUrl.substring(0, gitUrl.length() - 4);
       repoUrl = maskAccessToken(repoUrl);
       buildDetails.setRepoLoc(repoUrl);
       buildDetails.addAdditionalDetails(BuildDetails.scmType, gitUrl.contains("gitlab.com") ? SCMConstants.GITLAB : (gitUrl.contains("github.com") ? SCMConstants.GITHUB : "GIT"));
@@ -23,20 +24,61 @@ public class SCMDetails {
       buildDetails.setCommitSha(env.get(SCMConstants.GitCommit));
       if (gitUrl.contains("gitlab.com") || gitUrl.contains("github.com")) {
         try {
-          String repoFullName = new URL(repoUrl).getPath().replaceFirst("/","");
+          String repoFullName = new URL(repoUrl).getPath().replaceFirst("/", "");
           buildDetails.addRepoDetails(SCMConstants.RepoFullName, repoFullName);
           buildDetails.addRepoDetails(SCMConstants.RepoName, repoFullName.split("/")[1]);
-        } catch (Exception e){
+        } catch (Exception e) {
           LOGGER.log(Level.SEVERE, "Unable to fetch the path for repo url ::" + repoUrl);
         }
         buildDetails.addRepoDetails(SCMConstants.RepoUrl, repoUrl);
       }
     }
     String svnUrl = env.get(SCMConstants.SvnUrl);
-    if(svnUrl!=null){
+    if (svnUrl != null) {
       buildDetails.setRepoLoc(svnUrl);
       buildDetails.addAdditionalDetails(BuildDetails.scmType, SCMConstants.SVN);
       buildDetails.setCommitSha(env.get(SCMConstants.SvnRevision));
+    }
+  }
+
+  public static void populateSCMDetails(EnvVars env, BuildDetails buildDetails, JSONObject configJson) {
+
+    if (configJson != null) {
+      JSONObject flowDetails = configJson.optJSONObject("flow-definition");
+      if (flowDetails != null) {
+        JSONObject properties = flowDetails.optJSONObject("properties");
+        if (properties != null) {
+          JSONObject multiBranchProperty = properties.optJSONObject("org.jenkinsci.plugins.workflow.multibranch.BranchJobProperty");
+          if (multiBranchProperty != null) {
+            JSONObject branch = multiBranchProperty.optJSONObject("branch");
+            if (branch != null) {
+              JSONObject scm = branch.optJSONObject("scm");
+              if (scm != null) {
+                JSONObject userRemoteConfigs = scm.optJSONObject("userRemoteConfigs");
+                if (userRemoteConfigs != null) {
+                  JSONObject config = userRemoteConfigs.optJSONObject("hudson.plugins.git.UserRemoteConfig");
+                  if (config != null) {
+                    String repoUrl = config.optString("url");
+                    buildDetails.setRepoLoc(repoUrl);
+                    buildDetails.addAdditionalDetails(BuildDetails.scmType, repoUrl.contains("gitlab.com") ? SCMConstants.GITLAB : (repoUrl.contains("github.com") ? SCMConstants.GITHUB : "GIT"));
+                    if (repoUrl.contains("gitlab.com") || repoUrl.contains("github.com")) {
+                      try {
+                        String repoFullName = new URL(repoUrl).getPath().replaceFirst("/", "");
+                        buildDetails.addRepoDetails(SCMConstants.RepoFullName, repoFullName);
+                        buildDetails.addRepoDetails(SCMConstants.RepoName, repoFullName.split("/")[1]);
+                      } catch (Exception e) {
+                        LOGGER.log(Level.SEVERE, "Unable to fetch the path for repo url ::" + repoUrl);
+                      }
+                      buildDetails.addRepoDetails(SCMConstants.RepoUrl, repoUrl);
+                      buildDetails.setBranchName(env.get(SCMConstants.BranchName));
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 
